50b13d88702df7445d9a5eb704c721bd61b84dff
Robert Emprechtinger <robert.emprechtinger@donau-uni.ac.at>
2021-08-11 14:28:38 +0200

    Initial commit

---
+++ R/barinfo.R
@@ -0,0 +1,9 @@
+library(grid)
+
+box_left <- rectGrob(x=0.45, y=0.5, height = 0.1, width=0.4, just="right")
+box_right <- rectGrob(x=0.55, y=0.5, height = 0.1, width=0.4, just="left")
+
+
+boxes <- gTree(children = gList(box_left, box_right))
+grid.draw(boxes)
+grid.remove(boxes)
