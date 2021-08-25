060557c284ab0ee1a4b00794fdef7d4e160708f2
Robert Emprechtinger <robert.emprechtinger@donau-uni.ac.at>
2021-08-25 14:50:16 +0200

    try to fix textGrob error

--- R/barinfo.R
+++ R/barinfo.R
@@ -1,19 +1,4 @@
-library(grid)
-library(gridExtra)
-
-#outcomes
-n <- 10000
-cases_right <- c(600, 200, 100, 100)
-cases_left <- c(600, 400, 100, 100)
-outcome_texts <- c("Really bad headache", "Headache. Not so bad. But still...", "test 3", "test4")
-headline_main_text_left <- "Placebo"
-headline_main_text_right <- "Einhornstaub"
-headline_1_text_left <- "erhielten"
-headline_2_text_left <- "Personen"
-headline_2_text_right <- headline_2_text_left
-
-
-#' Title
+#' Create infoplot with bars
 #'
 #' @param n the length of the empty bars
 #' @param cases_right number of cases right
--- tests/testthat/test-barinfo.R
+++ tests/testthat/test-barinfo.R
@@ -3,6 +3,19 @@ test_that("multiplication works", {
 })
 
 
+#outcomes
+n <- 10000
+cases_right <- c(600, 200, 100, 100)
+cases_left <- c(600, 400, 100, 100)
+outcome_texts <- c("Really bad headache", "Headache. Not so bad. But still...", "test 3", "test4")
+headline_main_text_left <- "Placebo"
+headline_main_text_right <- "Einhornstaub"
+headline_1_text_left <- "erhielten"
+headline_2_text_left <- "Personen"
+headline_2_text_right <- headline_2_text_left
+
+
+
 infobar(headline_main_text_left, headline_main_text_right,
         headline_1_text_left, headline_2_text_left,
         headline_2_text_right, n, outcome_texts, cases_right, cases_left,
